554dbd4d4c50b2f6880a18c3993ffedccd38b348
Wilbur <wilburtw@amazon.com>
2020-12-02 14:33:34 -0800

    Part 1 completed

--- 02_day/02_day.py
+++ 02_day/02_day.py
@@ -5,19 +5,37 @@ def day_02_challenge_part_1():
     input_file = "input_02_day.txt"
 
     with open(input_file, "r") as file:
-        prev_lines = []
-        line = int(file.readline().strip())
+        # 7-8 k: hgcjkxhkskk
+        line = file.readline().strip()
+        num_passwords = 0
 
         while line:
-            prev_lines.append(line)
-            line = int(file.readline().strip())
+            range_nums, letter, password = line.split(" ")
 
-    return 0
-    
+            min, max = range_nums.split("-")
+            min = int(min)
+            max = int(max)
+            letter = letter[0]
+
+            count = 0
+            for chr in password:
+                print(chr, " count: ", count, "\n")
+                if chr == letter:
+                    count += 1
+            if count < min or count > max:
+                # 1-3 a means it must contain a at least 1 time and at most 3 times
+                num_passwords += 0
+            else:
+                num_passwords += 1
+
+            line = file.readline().strip()
+
+    return num_passwords
+
 
 def main():
     print(day_02_challenge_part_1())
-    
+
 
 if __name__ == "__main__":
     main()
