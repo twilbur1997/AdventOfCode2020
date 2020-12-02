d1fe875ff94563da802a38e2ef87e1ecbc7469ed
Wilbur <wilburtw@amazon.com>
2020-12-02 15:00:07 -0800

    Finished Part 2 and printed answers

--- 02_day/02_day.py
+++ 02_day/02_day.py
@@ -19,7 +19,6 @@ def day_02_challenge_part_1():
 
             count = 0
             for chr in password:
-                print(chr, " count: ", count, "\n")
                 if chr == letter:
                     count += 1
             if count < min or count > max:
@@ -33,8 +32,38 @@ def day_02_challenge_part_1():
     return num_passwords
 
 
+def day_02_challenge_part_2():
+    input_file = "input_02_day.txt"
+
+    with open(input_file, "r") as file:
+        line = file.readline().strip()
+        num_passwords = 0
+
+        while line:
+            index_nums, letter, password = line.split(" ")
+
+            ind1, ind2 = index_nums.split("-")
+            ind1 = int(ind1)-1 # (Be careful; No concept of "index zero"!)
+            ind2 = int(ind2)-1
+            letter = letter[0]
+
+            count = 0
+            if password[ind1] == letter:
+                count += 1
+            if password[ind2] == letter:
+                count += 1
+
+            if count == 1:
+                num_passwords += 1
+
+            line = file.readline().strip()
+
+    return num_passwords
+
+
 def main():
-    print(day_02_challenge_part_1())
+    print("Day 2 Part 1: ", day_02_challenge_part_1())
+    print("Day 2 Part 2: ", day_02_challenge_part_2())
 
 
 if __name__ == "__main__":
