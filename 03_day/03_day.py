715f0c2563186898802e439bac0c4671ae8df819
Wilbur <wilburtw@amazon.com>
2020-12-03 13:48:57 -0800

    Part 1 bugged. Wrong answer of 70

--- 03_day/03_day.py
+++ 03_day/03_day.py
@@ -1,23 +1,32 @@
-import os
-
-
 def day_03_challenge_part_1():
     input_file = "input_03_day.txt"
 
     with open(input_file, "r") as file:
-        prev_lines = []
-        line = int(file.readline().strip())
+        # ......#..##..#...#...#.###.....
+        # following a slope of right 3 and down 1,
+        # how many trees would you encounter?
+        line = file.readline() # skip first line
+        width = len(line)
 
+        num_trees = 0
+        x_pos = 0
+
+        x_pos = (x_pos + 3) % width
+        line = file.readline()
         while line:
-            prev_lines.append(line)
-            line = int(file.readline().strip())
+            if line[x_pos] == "#":
+                num_trees += 1
 
-    return 0
-    
+            x_pos = (x_pos + 3) % width
+            line = file.readline()
+
+    return num_trees
+
 
 def main():
-    print(day_03_challenge_part_1())
-    
+    print("Day 3 Part 1: ", day_03_challenge_part_1())
+    # print("Day 3 Part 2: ", day_03_challenge_part_2())
+
 
 if __name__ == "__main__":
     main()
