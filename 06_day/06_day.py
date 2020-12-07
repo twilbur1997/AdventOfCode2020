484891ca331059443d8151cefb49525a67a4be40
Wilbur <wilburtw@amazon.com>
2020-12-06 23:08:39 -0800

    Fixed part 2 by intersecting the sets while reading them in. Got 3305 instead of 3310.

--- 06_day/06_day.py
+++ 06_day/06_day.py
@@ -29,19 +29,21 @@ def day_06_challenge_part_2():
     with open(input_file, "r") as file:
         line = file.readline()
         num_questions = 0
-        list_indiv_sets = []
+        family_set = {0}-{0}
+        prev_line = 0
         while line:
             if line == "\n":
-                family_set = list_indiv_sets[0]
-                for indiv_set in list_indiv_sets:
-                    family_set = family_set.intersection(indiv_set)
                 num_questions += len(family_set)
-                list_indiv_sets = []
+                print(len(family_set), "\n\n")
+                family_set = {0}-{0}
             else:
                 indiv_set = {0}-{0}
                 for char in line.strip():
                     indiv_set.add(char)
-                list_indiv_sets.append(indiv_set)
+                if prev_line == "\n" or prev_line == 0:
+                    family_set = indiv_set
+                else:
+                    family_set = family_set.intersection(indiv_set)
 
             prev_line = line
             line = file.readline()
