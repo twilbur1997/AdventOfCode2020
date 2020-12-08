5a95ee7ac0b3b49da72b1be1c23706a44767479a
Wilbur <wilburtw@amazon.com>
2020-12-07 21:32:59 -0800

    Revert "Still haven't solved Day 7 part 2, took out the deepcopy since that was taking 897 seconds to run, and it didn't change my answer."
    This reverts commit 1cd2c63b9046ba2404f732e8c5b39e38841ac8ed.

--- 07_day/07_day.py
+++ 07_day/07_day.py
@@ -1,5 +1,4 @@
 import copy
-import time
 
 
 def generate_luggage():
@@ -32,49 +31,40 @@ def generate_luggage():
     return luggage_dict
 
 
-def start_recurse_bags(luggage, find_bag, count_bags):
+def start_recurse_bags(luggage, find_bag):
     # Find how many parent bags can eventually fit "find_bag"
     total_bags = 0
     for parent_bag in luggage.keys():
-        total_bags += recurse_bags(luggage, parent_bag, find_bag, count_bags)
+        total_bags += recurse_bags(luggage, parent_bag, find_bag)
         print(total_bags)
 
     return total_bags
 
 
-def recurse_bags(luggage, parent_bag, find_bag, count_bags):
+def recurse_bags(luggage, parent_bag, find_bag):
     total = 0
     if parent_bag not in luggage: # due to del later, doesn't affect top level
         return 0
     children_bags = luggage[parent_bag]
     for pair in children_bags:
         if pair[1] == find_bag:
-            if count_bags:
-                return pair[0]
-            else:
-                return 1
+            return 1
 
         # prevent infinite loops
-        # luggage2 = copy.deepcopy(luggage)
-        # del luggage2[parent_bag]
-        total += pair[0]*recurse_bags(luggage, pair[1], find_bag, count_bags)
+        luggage2 = copy.deepcopy(luggage)
+        del luggage2[parent_bag]
+        total += recurse_bags(luggage2, pair[1], find_bag)
 
-    if count_bags:
-        return total
-    else:
-        return (total > 0)
+    return (total > 0)
 
 
-def day_07_challenge_part_1(input_bag, count_bags):
+def day_07_challenge_part_1(input_bag):
     luggage_dict = generate_luggage()
-    return start_recurse_bags(luggage_dict, input_bag, count_bags)
+    return start_recurse_bags(luggage_dict, input_bag)
 
 
 def main():
-    start = time.time()
-    # print(day_07_challenge_part_1("shiny gold", False))
-    print(day_07_challenge_part_1("shiny gold", True))
-    print("Time elapsed: ", time.time() - start)
+    print(day_07_challenge_part_1("shiny gold"))
 
 
 if __name__ == "__main__":
