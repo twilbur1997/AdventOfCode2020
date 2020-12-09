a701728be0a04d278662f3b736594725fc69966f
Wilbur <wilburtw@amazon.com>
2020-12-08 17:50:57 -0800

    Started on Part 1, but got stuck. Count may not be jumping correctly.

--- 08_day/08_day.py
+++ 08_day/08_day.py
@@ -10,9 +10,31 @@ def read_instructions():
 
     return instructions
 
+
+def process_instruction(line):
+    command, number = line.split(" ")
+    number = int(number)
+    return command, number
+
+
 def day_08_challenge_part_1():
     instructions = read_instructions()
+    count = 0
+    accumulator = 0
+    prev_counts = {0}-{0}
+    while count < len(instructions):
+        if count in prev_counts:
+            return accumulator
+        prev_counts.add(count)
+        cmd, num = process_instruction(instructions[count])
+        if cmd == "acc":
+            accumulator += num
+        elif cmd == "jmp":
+            count += num
+            continue
+        count += 1
 
+    return 0
 
 
 def main():
