428f7653015da92fc336386ce89db11a49980fc6
Tommy Wilbur <wilburtw@amazon.com>
2020-12-23 14:08:57 -0800

    Commented print statements and made instruction reading easier.

--- 08_day/08_day.py
+++ 08_day/08_day.py
@@ -1,16 +1,3 @@
-def read_instructions():
-    input_file = "input_08_day.txt"
-    instructions = []
-
-    with open(input_file, "r") as file:
-        line = file.readline().strip()
-        while line:
-            instructions.append(line)
-            line = file.readline().strip()
-
-    return instructions
-
-
 def get_cmd_num(line):
     command, number = line.split(" ")
     # print(line.split(" "))
@@ -19,13 +6,13 @@ def get_cmd_num(line):
 
 
 def day_08_challenge_part_1():
-    instructions = read_instructions()
+    instructions = [line.strip() for line in open('input_08_day.txt', 'r')]
     count = 0
     accumulator = 0
     prev_counts = {0}-{0}
     while count < len(instructions):
-        print("Count: ", count)
-        print("Acc: ", accumulator)
+        # print("Count: ", count)
+        # print("Acc: ", accumulator)
         cmd, num = get_cmd_num(instructions[count])
 
         if count in prev_counts:
