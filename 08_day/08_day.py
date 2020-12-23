5fdd6845dc2722011034c33a6b9a00865eae1058
Tommy Wilbur <wilburtw@amazon.com>
2020-12-23 14:22:23 -0800

    Finished Part 2.

--- 08_day/08_day.py
+++ 08_day/08_day.py
@@ -25,10 +25,53 @@ def day_08_challenge_part_1():
 
     return 0
 
+def day_08_challenge_part_2():
+    instructions = [line.strip() for line in open('input_08_day.txt', 'r')]
+    count = 0
+    accumulator = 0
+    prev_counts = {0}-{0}
+    while count < len(instructions):
+        cmd, num = get_cmd_num(instructions[count])
+
+        if count in prev_counts:
+            break
+        prev_counts.add(count)
+
+        accumulator += num*(cmd == "acc")
+        count += num*(cmd == "jmp")
+        count += (cmd != "jmp")
+
+    for prev in prev_counts:
+        count = 0
+        accumulator = 0
+        prev_counts_2 = {0}-{0}
+        while count < len(instructions):
+            cmd, num = get_cmd_num(instructions[count])
+
+            # Let's try switching the command that we already ran earlier
+            if count == prev and cmd != "acc":
+                # value_if_true if condition else value_if_false
+                cmd = "nop" if cmd == "jmp" else "jmp"
+
+            if count in prev_counts_2:
+                break
+            prev_counts_2.add(count)
+
+            accumulator += num*(cmd == "acc")
+            count += num*(cmd == "jmp")
+            count += (cmd != "jmp")
+
+        if count in prev_counts_2:
+            continue
+        else:
+            print(count)
+            print(len(instructions))
+            print(accumulator)
+
 
 def main():
-    print(day_08_challenge_part_1())
-    # print(day_08_challenge_part_2())
+    # print(day_08_challenge_part_1())
+    print(day_08_challenge_part_2())
 
 
 if __name__ == "__main__":
