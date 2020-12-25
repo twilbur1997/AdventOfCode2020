fb2ec49f2427f577fc496b9ad7f06e2c8bab49bf
Tommy Wilbur <wilburtw@amazon.com>
2020-12-24 17:22:11 -0800

    First try on part 1, almost done

--- 11_day/11_day.py
+++ 11_day/11_day.py
@@ -1,23 +1,75 @@
-import os
+def get_neighbors(x_coord, y_coord, seat_list):
+    # NW, N, NE, E, SE, S, SW, W
+    total = 0
+
+    if y_coord > 0:
+        if x_coord > 0:
+            total += (seat_list[y_coord-1][x_coord-1] == "#")
+        total += (seat_list[y_coord-1][x_coord] == "#")
+        if x_coord < len(seat_list[y_coord-1])-1:
+            total += (seat_list[y_coord-1][x_coord+1] == "#")
+
+    if x_coord > 0:
+        total += (seat_list[y_coord][x_coord-1] == "#")
+    # total += (seat_list[y_coord][x_coord] == "#") # Nope, isn't a neighbor
+    if x_coord < len(seat_list[y_coord])-1:
+        total += (seat_list[y_coord][x_coord+1] == "#")
+
+    if y_coord < len(seat_list)-1:
+        if x_coord > 0:
+            total += (seat_list[y_coord+1][x_coord-1] == "#")
+        total += (seat_list[y_coord+1][x_coord] == "#")
+        if x_coord < len(seat_list[y_coord+1])-1:
+            total += (seat_list[y_coord+1][x_coord+1] == "#")
+    return total
+
+
+def change_seat_status(x_coord, y_coord, seat_list, occupied_status):
+    neighbors = get_neighbors(x_coord, y_coord, seat_list)
+    if occupied_status and neighbors >= 4:
+        return True
+    elif not occupied_status and neighbors == 0:
+        return True
+    return False
 
 
 def day_11_challenge_part_1():
-    input_file = "input_11_day.txt"
+    seat_list = [line.strip() for line in open('input_11_day.txt', 'r')]
 
-    with open(input_file, "r") as file:
-        prev_lines = []
-        line = int(file.readline().strip())
+    new_seats = []
+    y_coord = 0
+    for seat_string in seat_list:
+        new_seat_string = []
+        x_coord = 0
+        for seat in seat_string:
+            if seat == ".":
+                new_seats.append(".")
 
-        while line:
-            prev_lines.append(line)
-            line = int(file.readline().strip())
+            elif seat == "L":
+                if change_seat_status(x_coord, y_coord, seat_list, False):
+                    new_seats.append("#")
+                else:
+                    new_seats.append("L")
+
+            elif seat == "#":
+                if change_seat_status(x_coord, y_coord, seat_list, True):
+                    new_seats.append("L")
+                else:
+                    new_seats.append("#")
+            else:
+
+            x_coord += 1
+
+        y_coord += 1
+
+
 
     return 0
-    
+
 
 def main():
     print(day_11_challenge_part_1())
-    
+
 
 if __name__ == "__main__":
     main()
