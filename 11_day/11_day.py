d31faeae3f48e98c92360592a931bd8579f28eba
Tommy Wilbur <wilburtw@amazon.com>
2020-12-27 11:34:56 -0800

    Took out y conditional above. Still getting 4559: too high

--- 11_day/11_day.py
+++ 11_day/11_day.py
@@ -1,7 +1,6 @@
 def get_neighbors(x_coord, y_coord, seat_list):
     # NW, N, NE, E, SE, S, SW, W
     total = 0
-    print(x_coord, y_coord)
 
     if y_coord > 0:
         if x_coord > 0:
@@ -10,12 +9,11 @@ def get_neighbors(x_coord, y_coord, seat_list):
         if x_coord < len(seat_list[y_coord-1])-1:
             total += (seat_list[y_coord-1][x_coord+1] == "#")
 
-    if y_coord < len(seat_list):
-        if x_coord > 0:
-            total += (seat_list[y_coord][x_coord-1] == "#")
-        # total += (seat_list[y_coord][x_coord] == "#") # Nope, isn't a neighbor
-        if x_coord < len(seat_list[y_coord])-1:
-            total += (seat_list[y_coord][x_coord+1] == "#")
+    if x_coord > 0:
+        total += (seat_list[y_coord][x_coord-1] == "#")
+    # total += (seat_list[y_coord][x_coord] == "#") # Nope, isn't a neighbor
+    if x_coord < len(seat_list[y_coord])-1:
+        total += (seat_list[y_coord][x_coord+1] == "#")
 
     if y_coord < len(seat_list)-1:
         if x_coord > 0:
@@ -38,7 +36,7 @@ def change_seat_status(x_coord, y_coord, seat_list, occupied_status):
 def day_11_challenge_part_1():
     seat_list = [line.strip() for line in open('input_11_day.txt', 'r') if line != None]
 
-    seats_changing = True
+    seats_changing = 10
     while seats_changing:
         new_seats = []
         y_coord = 0
@@ -67,15 +65,15 @@ def day_11_challenge_part_1():
             new_seats.append(new_seat_string)
             y_coord += 1
 
-
-        seats_changing = False # We should be good, let's stop here... wait...
         changed = 0
         for seat_str_new, seat_str_old in zip(new_seats, seat_list):
             if seat_str_new != seat_str_old:
                 seat_list = new_seats
-                seats_changing = True
                 changed += 1
                 # break
+        if not changed:
+            seats_changing -= 1
+
         print(changed)
 
     total_occupied = 0
