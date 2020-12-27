b4ce2ee95d21501c6a0745d8073a48e30bda2eef
Tommy Wilbur <wilburtw@amazon.com>
2020-12-27 11:30:09 -0800

    4559 Too high.

--- 11_day/11_day.py
+++ 11_day/11_day.py
@@ -1,6 +1,7 @@
 def get_neighbors(x_coord, y_coord, seat_list):
     # NW, N, NE, E, SE, S, SW, W
     total = 0
+    print(x_coord, y_coord)
 
     if y_coord > 0:
         if x_coord > 0:
@@ -9,11 +10,12 @@ def get_neighbors(x_coord, y_coord, seat_list):
         if x_coord < len(seat_list[y_coord-1])-1:
             total += (seat_list[y_coord-1][x_coord+1] == "#")
 
-    if x_coord > 0:
-        total += (seat_list[y_coord][x_coord-1] == "#")
-    # total += (seat_list[y_coord][x_coord] == "#") # Nope, isn't a neighbor
-    if x_coord < len(seat_list[y_coord])-1:
-        total += (seat_list[y_coord][x_coord+1] == "#")
+    if y_coord < len(seat_list):
+        if x_coord > 0:
+            total += (seat_list[y_coord][x_coord-1] == "#")
+        # total += (seat_list[y_coord][x_coord] == "#") # Nope, isn't a neighbor
+        if x_coord < len(seat_list[y_coord])-1:
+            total += (seat_list[y_coord][x_coord+1] == "#")
 
     if y_coord < len(seat_list)-1:
         if x_coord > 0:
@@ -34,42 +36,47 @@ def change_seat_status(x_coord, y_coord, seat_list, occupied_status):
 
 
 def day_11_challenge_part_1():
-    seat_list = [line.strip() for line in open('input_11_day.txt', 'r')]
+    seat_list = [line.strip() for line in open('input_11_day.txt', 'r') if line != None]
 
-    new_seats = []
-    y_coord = 0
     seats_changing = True
     while seats_changing:
+        new_seats = []
+        y_coord = 0
         for seat_string in seat_list:
             new_seat_string = []
             x_coord = 0
             for seat in seat_string:
                 if seat == ".":
-                    new_seats.append(".")
+                    new_seat_string.append(".")
 
                 elif seat == "L":
                     if change_seat_status(x_coord, y_coord, seat_list, False):
-                        new_seats.append("#")
+                        new_seat_string.append("#")
                     else:
-                        new_seats.append("L")
+                        new_seat_string.append("L")
 
                 elif seat == "#":
                     if change_seat_status(x_coord, y_coord, seat_list, True):
-                        new_seats.append("L")
+                        new_seat_string.append("L")
                     else:
-                        new_seats.append("#")
+                        new_seat_string.append("#")
 
+                else: # Eh, just in case
+                    new_seat_string.append(seat)
                 x_coord += 1
             new_seats.append(new_seat_string)
-
             y_coord += 1
-        seats_changing = False
+
+
+        seats_changing = False # We should be good, let's stop here... wait...
+        changed = 0
         for seat_str_new, seat_str_old in zip(new_seats, seat_list):
             if seat_str_new != seat_str_old:
                 seat_list = new_seats
                 seats_changing = True
-                continue
-        break
+                changed += 1
+                # break
+        print(changed)
 
     total_occupied = 0
     for seat_str in new_seats:
